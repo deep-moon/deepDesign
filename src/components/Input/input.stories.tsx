353b5712ae695470ac2d6887ac4c4998c8e05f0e
deep-moon <shenchuang1997@gmail.com>
2022-10-20 07:26:30 +0800

    input组件开发

---
+++ src/components/Input/input.stories.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Input from "./input";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+// import mdx from "./button.mdx";
+import Button from "../Button/button";
+
+const InputMeta: ComponentMeta<typeof Input> = {
+  title: "Input",
+  id: "Input",
+  component: Input,
+};
+export default InputMeta;
+
+const Template: ComponentStory<typeof Input> = (args) => (
+  <Input {...args}></Input>
+);
+export const Default = Template.bind({});
+
+export const Small = Template.bind({});
+Small.args = {
+  size: "sm",
+};
+export const Large = Template.bind({});
+Large.args = {
+  size: "lg",
+};
+export const inputWithIcon = Template.bind({});
+inputWithIcon.args = {
+  icon: "coffee",
+};
+inputWithIcon.storyName = "带图标的input";
+export const inputWithDisabled = Template.bind({});
+inputWithDisabled.args = {
+  disabled: true,
+};
+inputWithDisabled.storyName = "被禁用的input";
+
+export const inputWithAddonBefore = Template.bind({});
+inputWithAddonBefore.args = {
+  addonBefore: "https://",
+};
+inputWithAddonBefore.storyName = "带前缀的input";
+
+export const inputWithAddonAfter = Template.bind({});
+inputWithAddonAfter.args = {
+  addonAfter: ".com",
+};
+inputWithAddonAfter.storyName = "带后缀的input";
---
+++ src/components/Input/input.tsx
@@ -0,0 +1,53 @@
+import React, { ReactElement, InputHTMLAttributes } from "react";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import classNames from "classnames";
+import Icon from "../Icon/icon";
+type InputSize = "lg" | "sm";
+interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, "size"> {
+  /**是否禁用 Input */
+  disabled?: boolean;
+  /**设置 input 大小，支持 lg 或者是 sm */
+  size?: InputSize;
+  /**添加图标，在右侧悬浮添加一个图标，用于提示 */
+  icon?: IconProp;
+  /**添加前缀 用于配置一些固定组合 */
+  addonBefore?: string | ReactElement;
+  /**添加后缀 用于配置一些固定组合 */
+  addonAfter?: string | ReactElement;
+}
+
+/**
+ * Input 输入框 通过鼠标或键盘输入内容，是最基础的表单域的包装。
+ *
+ * 支持 HTMLInput 的所有基本属性
+ */
+export const Input: React.FC<InputProps> = (props) => {
+  const { disabled, size, icon, addonBefore, addonAfter, style, ...restProps } =
+    props;
+  const classes = classNames("deep-input-wrapper", {
+    [`input-${size}`]: size,
+    "input-group": addonBefore || addonAfter,
+    "input-group-addonBefore": addonBefore,
+    "input-group-addonAfter": addonAfter,
+    disabled: disabled,
+  });
+  return (
+    <div className={classes} style={style}>
+      {addonBefore && (
+        <div className="deep-input-group-addonBefore">{addonBefore}</div>
+      )}
+      {icon && (
+        <div className="icon-wrapper">
+          <Icon icon={icon} className="input-icon"></Icon>
+        </div>
+      )}
+      <input className="deep-input-inner" disabled={disabled} {...restProps} />
+
+      {addonAfter && (
+        <div className="deep-input-group-addonAfter">{addonAfter}</div>
+      )}
+    </div>
+  );
+};
+
+export default Input;
